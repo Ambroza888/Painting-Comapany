40df0b3acd8a33f43ffc08473d8512f548c714db
unknown <Ambroza8888@gmail.com>
2020-02-20 15:35:49 -0800

    Reviews

--- Models/Review.cs
+++ Models/Review.cs
@@ -8,7 +8,8 @@ namespace Paiting
     {
         public int ReviewId {get;set;}
         public string Name {get;set;}
-        
+        public string Text {get;set;}
+        public int Rating {get;set;}
         public DateTime CreatedAt {get;set;} = DateTime.Now;
         public DateTime UpdatedAt {get;set;} = DateTime.Now;
     }
