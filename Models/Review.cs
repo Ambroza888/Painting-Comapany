572c7ab909d0c07bdc4f72da5b31be9e0954fe56
unknown <Ambroza8888@gmail.com>
2020-02-25 13:47:31 -0800

    Reviews css done

--- Models/Review.cs
+++ Models/Review.cs
@@ -7,11 +7,19 @@ namespace Paiting
     public class Review
     {
         public int ReviewId {get;set;}
+        [Required]
+        [MinLength(2,ErrorMessage="Your name needs to be more than 2 characters")]
         public string FirstName {get;set;}
+        [Required]
         public string LastName {get;set;}
+        [Required]
+        [MinLength(2,ErrorMessage="Comment needs to be more than 2 characters")]
         public string Text {get;set;}
+        [Required]
+        [Range(1,5)]
         public int Rating {get;set;}
-        public int Area {get;set;}
+        [Required]
+        public string Area {get;set;}
         public DateTime CreatedAt {get;set;} = DateTime.Now;
         public DateTime UpdatedAt {get;set;} = DateTime.Now;
     }
