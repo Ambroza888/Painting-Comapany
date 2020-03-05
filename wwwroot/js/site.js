f089ec426e6e8ec0c0c3ebbd89cc8d37619f9a9f
unknown <Ambroza8888@gmail.com>
2020-03-04 17:10:30 -0800

    Modal form

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -52,4 +52,10 @@ $(function() {
   }
 
   // < ----- Button for clicking to open MODAL LOGIN ---- >
-
+  var modal = document.getElementById('id01'); 
+  
+  window.onclick = function(event) { 
+      if (event.target == modal) { 
+          modal.style.display = "none"; 
+      } 
+  } 
